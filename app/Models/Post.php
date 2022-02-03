c6a88b56bffa7084b2afc4f81353b69d2d4687d3
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-02-03 16:52:36 +0200

    Making migrations and relationships

---
+++ app/Models/Post.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+
+    public function category()
+    {
+        $this->belongsTo(Category::class);
+    }
+
+    public function author()
+    {
+        $this->belongsTo(User::class, 'user_id');
+    }
+}
