259b030d526ebecc8cd3a8e0567ac4b553f0044f
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-02-07 19:25:58 +0200

    installing Vue Router

--- resources/views/posts/__header.blade.php
+++ resources/views/posts/__header.blade.php
@@ -18,7 +18,18 @@
     <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
         <!--  Category -->
         <div class="override relative flex lg:inline-flex items-center bg-blue-100 rounded-xl">
-            <select
+
+            <div>
+                <button>Categories</button>
+
+                <a href="#">One</a>
+                <a href="#">Two</a>
+                <a href="#">Three</a>
+
+            </div>
+
+
+            {{-- <select
                 class="border-none outline-hidden flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
                 <option value="category" disabled selected>Category</option>
                 <option value="personal">Personal</option>
@@ -32,7 +43,7 @@
                     <path fill="#222" d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
                     </path>
                 </g>
-            </svg>
+            </svg> --}}
         </div>
 
         <!-- Search -->
