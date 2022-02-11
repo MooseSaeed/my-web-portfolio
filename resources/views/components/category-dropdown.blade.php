350305122be8eb87766a4545d667bf1fb3e26a8c
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-02-11 14:12:56 +0200

    adding all link to categories

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -2,13 +2,19 @@
 
     <Dropdowncontent>
 
-        @foreach ($categories as $category)
-            <Dropdownitems
-                href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
-                class="{{ isset($currentCategory) && $currentCategory->is($category) ? ' selectedCategoryItem' : '' }}">
-                {{ $category->name }}
-            </Dropdownitems>
-        @endforeach
+        <Dropdownitems href="/">
+            All
+        </Dropdownitems>
+
+        <div>
+            @foreach ($categories as $category)
+                <Dropdownitems
+                    href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
+                    class="{{ isset($currentCategory) && $currentCategory->is($category) ? ' selectedCategoryItem' : '' }}">
+                    {{ $category->name }}
+                </Dropdownitems>
+            @endforeach
+        </div>
 
     </Dropdowncontent>
 
