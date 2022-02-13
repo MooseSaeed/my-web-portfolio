97f610c2805d3d655261654d74d8d9cfcbecc307
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-02-13 22:32:32 +0200

    create commenting form

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CommentFactory extends Factory
@@ -16,7 +17,7 @@ class CommentFactory extends Factory
     {
         return [
             'post_id' => Post::factory(),
-            'user_id' => Post::factory(),
+            'user_id' => User::factory(),
             'body' => $this->faker->paragraph(3)
         ];
     }
