125346c37d1947d1356d0c3654882baa598893e6
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-02-13 18:19:39 +0200

    comments markup

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,22 @@
+<article class="flex bg-gray-100 p-6 rounded-xl border-gray-200 space-x-4">
+
+    <div class="flex-shrink-0 ">
+        <img src="https://i.pravatar.cc/60" width="60" height="60" class="rounded-xl" alt="">
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">Posted
+                <time>8 months ago</time>
+            </p>
+        </header>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ipsa consequuntur
+            doloremque debitis odio, eligendi repellendus fugit officia in vel voluptatem sunt
+            corrupti ut deleniti totam eos! Asperiores eius nihil nobis numquam expedita fuga
+            laudantium praesentium? Modi voluptatum eius.</p>
+
+    </div>
+
+</article>
