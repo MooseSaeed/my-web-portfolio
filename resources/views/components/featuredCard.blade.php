fbacf90490d62723ab58fb26cc20dd650ac897c5
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-02-20 20:32:43 +0200

    removing author because it's only me in here

--- resources/views/components/featuredCard.blade.php
+++ resources/views/components/featuredCard.blade.php
@@ -1,12 +1,10 @@
 @props(['post'])
 
 <card class="rounded-xl">
-    <article
-        class="transition-colors duration-300 hover:bg-blue-50 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
+    <article class="transition-colors duration-300 hover:bg-blue-50 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
         <div class="py-6 px-5 lg:flex">
             <div class="flex-1 lg:mr-8">
-                <img src="{{ asset('storage/' . $post->thumbnail) }}" alt="Blog Post illustration"
-                    class="rounded-xl object-cover" />
+                <img src="{{ asset('storage/' . $post->thumbnail) }}" alt="Blog Post illustration" class="rounded-xl object-cover" />
             </div>
 
             <div class="flex-1 flex flex-col justify-between">
@@ -20,42 +18,29 @@
                         </a>
 
                         <span class="mt-4 block text-gray-400 text-xs">
-                            Published <time>{{ $post->created_at->diffForHumans() }}</time> in <span
-                                class="space-x-2">
-                                <a href="/?category={{ $post->category->slug }}"
-                                    class="ml-1 px-6 py-1 border border-violet-400 rounded-full text-violet-400 text-xs uppercase font-semibold"
-                                    style="font-size: 10px">{{ $post->category->name }}</a>
+                            Published <time>{{ $post->created_at->diffForHumans() }}</time> in <span class="space-x-2">
+                                <a href="/?category={{ $post->category->slug }}" class="ml-1 px-6 py-1 border border-violet-400 rounded-full text-violet-400 text-xs uppercase font-semibold" style="font-size: 10px">{{ $post->category->name }}</a>
                             </span>
                         </span>
                     </div>
                 </header>
 
-                <div class="card-info text-sm mt-2">
+                <div class="card-info text-sm mt-4">
                     <p>
                         {!! $post->excerpt !!}
                     </p>
                 </div>
 
                 <footer class="flex flex-col sm:flex-row justify-between items-center mt-8">
-                    <div class="flex items-center text-sm">
-                        <a href="/?author={{ $post->author->username }}">
-                            <img src="/images/My Logo Avatar.png" alt="Logo avatar" />
-                        </a>
-                        <div class="ml-3">
-                            <a href="/?author={{ $post->author->username }}">
-                                <h5 class="font-bold">{{ $post->author->name }}</h5>
-                            </a>
-                        </div>
-                    </div>
+                    <div></div>
 
-                    <div class="">
-                        <a href="/posts/{{ $post->slug }}"
-                            class="demogard transition duration-300 font-semibold 
+
+                    <a href=" /posts/{{ $post->slug }}" class="demogard transition duration-300 font-semibold 
                 bg-gradient-to-r from-purple-600 to-blue-500
                 hover:bg-gradient-to-bl focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800
                  text-white rounded-full py-2 px-8 text-xs">Read
-                            More</a>
-                    </div>
+                        More</a>
+
                 </footer>
             </div>
         </div>
--- resources/views/components/postCard.blade.php
+++ resources/views/components/postCard.blade.php
@@ -21,30 +21,21 @@ hover:bg-blue-50 border border-black border-opacity-0 hover:border-opacity-5 rou
         </div>
     </header>
 
-    <div class="text-sm mt-4">
+    <div class="text-sm mt-8">
         <p> {!! $post->excerpt !!}</p>
     </div>
 
     <footer class="flex flex-col sm:flex-row lg:flex-col xl:flex-row justify-between items-center mt-8">
-        <div class="flex items-center text-sm">
-            <a href="/?author={{ $post->author->username }}">
-                <img src="/images/My Logo Avatar.png" alt="Logo avatar" />
-            </a>
-            <div class="ml-3">
-                <a href="/?author={{ $post->author->username }}">
-                    <h5 class="font-bold">{{ $post->author->name }}</h5>
-                </a>
-            </div>
-        </div>
+        <div></div>
 
-        <div class="">
-            <a href="/posts/{{ $post->slug }}" class="demogard transition duration-300 font-semibold 
+
+        <a href="/posts/{{ $post->slug }}" class="demogard transition duration-300 font-semibold 
                 bg-gradient-to-r from-purple-600 to-blue-500
                 hover:bg-gradient-to-bl focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800
                text-white rounded-full py-2 px-8 text-xs">Read
-                More
-            </a>
-        </div>
+            More
+        </a>
+
 
     </footer>
 </article>
