c419bd452712587e75ee147ab07ecaa5d05f3d06
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-03-13 12:54:45 +0200

    migrate fresh and npm update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,40 +17,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        $user = User::factory()->create([
-            'name' => 'Mostafa Said',
-            'email' => 'mostafasaid1994@gmail.com',
-            'password' => '$2y$10$ReKXgbTVLecMg3Hjqwz1KeKb/Nm/QcGvUj7nEWKj3UpJaJKqwDavG',
-            'username' => 'MooseS94'
-        ]);
-
-        $frontend = Category::factory()->create([
-            'name' => 'Frontend',
-            'slug' => 'frontend'
-        ]);
-        $backend = Category::factory()->create([
-            'name' => 'Backend',
-            'slug' => 'backend'
-        ]);
-        $general = Category::factory()->create([
-            'name' => 'General',
-            'slug' => 'general'
-        ]);
-
-        Post::factory(4)->create([
-            'user_id' => $user,
-            'category_id' => $frontend,
-            'thumbnail' => 'thumbnails/d2hICXMFwUgsOFerDvNrrzp7cqTcGaBLaeArEj9Z.jpg'
-        ]);
-        Post::factory(4)->create([
-            'user_id' => $user,
-            'category_id' => $backend,
-            'thumbnail' => 'thumbnails/d2hICXMFwUgsOFerDvNrrzp7cqTcGaBLaeArEj9Z.jpg'
-        ]);
-        Post::factory(4)->create([
-            'user_id' => $user,
-            'category_id' => $general,
-            'thumbnail' => 'thumbnails/d2hICXMFwUgsOFerDvNrrzp7cqTcGaBLaeArEj9Z.jpg'
-        ]);
+        Post::factory(10)->create();
     }
 }
