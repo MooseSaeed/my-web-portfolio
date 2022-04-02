e5a0b25cc0e1448ce7f7b7d45d361f7ccb336098
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-04-02 09:02:41 +0200

    Updating Readme

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        Post::factory(10)->create();
+        /*         Post::factory(10)->create(
+            [
+                'title' => 'testing a big title here and now',
+                'thumbnail' => "thumbnails/3N0PE3I8kbKpqxyjrOKxRaXDAOoQWHTgjef3nTTf.png",
+                'excerpt' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, numquam maiores quibusdam aliquam quod tenetur facere, vel debitis optio impedit laudantium nostrum enim deleniti! Blanditiis laboriosam velit cupiditate suscipit quis. Assumenda libero excepturi incidunt, beatae debitis cumque saepe rem necessitatibus eum aut nam ducimus at eveniet cupiditate dolores, minus vitae?"
+            ]
+        ); */
     }
 }
